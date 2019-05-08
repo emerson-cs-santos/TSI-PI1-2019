47c8799b8a2d940aaeb4bfd232b8e0955226ed92
Emerson <syphonemerson@yahoo.com.br>
2019-05-07 21:57:24 -0300

    Correção na Transição de imagens
    Agora Transição de imagens faz opacidade correta.

--- Site Estático/Sem Bootstrap/JS/Slide_Show.js
+++ Site Estático/Sem Bootstrap/JS/Slide_Show.js
@@ -1,6 +1,6 @@
 // JavaScript dedicado ao controle de troca de imagens que são exibidas nos slides shows
 
-// Define foto inicial
+// Define foto inicial - TRABALHO
 function inicial()
 {
     // Define qual imagem será a inicial
@@ -21,7 +21,7 @@ function inicial()
     save('quantidade_maxima_de_fotos',qtd_maxima);
 }
 
-// Carrega Foto anterior em relação a atual
+// Carrega Foto anterior em relação a atual - TRABALHO
 function anterior()
 {
     // Carrega posição atual da foto
@@ -53,7 +53,7 @@ function anterior()
     save('trabalho',atual);
 }
 
-// Carrega Foto seguinte em relação a atual
+// Carrega Foto seguinte em relação a atual - TRABALHO
 function proximo()
 {
     // Carrega posição atual da foto
@@ -72,26 +72,23 @@ function proximo()
         atual = atual + 1;
     }
 
-    // Atualiza imagem
+    // Carrega Elemento da imagem
     var image = document.getElementById("img_Trabalhos");  
 
-    // "Apaga" imagem atual
-    //setTimeout(Troca_imagem_efeito(image,'apagar'),3000);
-    // Troca_imagem_efeito(image,'apagar');
+    image.classList.add('Efeito_troca_imagem_desaparece');
 
-    // Apaga imagem
     setTimeout(function () {
+        image.classList.remove('Efeito_troca_imagem_desaparece');
         image.src = trabalho_caminho(atual);
-        Troca_imagem_efeito(image,'apagar');
-    },3000);    
+    },2000);
 
-    // "Reaparece" na próxima imagem
+    image.classList.add('Efeito_troca_imagem_aparece');
+
     setTimeout(function () {
+        image.classList.remove('Efeito_troca_imagem_aparece');
         image.src = trabalho_caminho(atual);
-        Troca_imagem_efeito(image,'aparece');
-    },3000);
+    },2000);
 
-    //Troca_imagem_efeito(image,'aparece');
 
     // Atualiza caption da label
     document.getElementById("label_Trabalhos").innerHTML = trabalho_caption(atual);
@@ -100,37 +97,7 @@ function proximo()
     save('trabalho',atual);
 }
 
-// Efeito da Troca imagem
-function Troca_imagem_efeito(imagem,tipo)
-{
-   if(tipo == 'apagar'){
-    imagem.classList.add('top-desaparece');
-    //imagem.classList.remove('top-aparece');
-   }
-   else
-   {
-    //imagem.classList.add('top-aparece');
-    imagem.classList.remove('top-desaparece');
-   }
-}
-
-// Salva valor de variaval para uso posterior a executação do javascript
-function save(nome,valor)
-{
-    // Salvar informação localmente
-    localStorage.setItem(nome,valor);
-}
-
-// Carrega valor de variaval que se perdeu após a executação do javascript
-function load(nome)
-{
-    // Carregar informação local
-    var load = parseInt(localStorage.getItem(nome));
-
-    return(load);
-}
-
-// Carrega caminho da imagem pertinente a imagem
+// Carrega caminho da imagem pertinente a imagem - TRABALHO
 function trabalho_caminho(N_trabalho)
 {
    var caminho = '';
@@ -161,7 +128,7 @@ function trabalho_caminho(N_trabalho)
     return(caminho);
 }
 
-// Carrega caption da label referente a imagem
+// Carrega caption da label referente a imagem - TRABALHO
 function trabalho_caption(N_trabalho)
 {
     var caption = '';
@@ -190,4 +157,181 @@ function trabalho_caption(N_trabalho)
       }    
 
     return(caption);
+}
+
+
+// Define foto inicial - REUNIÒES
+function inicial()
+{
+    // Define qual imagem será a inicial
+    var imagem_inicial = 1;
+
+    // Define quantidade máxima de fotos
+    var qtd_maxima = 5;    
+    
+    var image = document.getElementById("img_Trabalhos");
+    image.src = trabalho_caminho(imagem_inicial); 
+
+    document.getElementById("label_Trabalhos").innerHTML = trabalho_caption(imagem_inicial);
+
+    // Salva imagem atual definida acima
+    save('trabalho',imagem_inicial);
+
+    // salva quantidade máxima de fotos
+    save('quantidade_maxima_de_fotos',qtd_maxima);
+}
+
+// Carrega Foto anterior em relação a atual - REUNIÒES
+function anterior()
+{
+    // Carrega posição atual da foto
+    var atual = load('trabalho');
+
+    // Carrega quantidade máxima de fotos
+    var maximo = load('quantidade_maxima_de_fotos');
+
+    // Se a foto atual for a primeira, ir para a última
+    if(atual == 1)
+    {
+        atual = maximo;
+    }
+    else // Subtrair mais 1 para seguir para a próxima imagem
+    {
+        atual = atual - 1;
+    }
+
+    // atualiza slide show
+    var image = document.getElementById("img_Trabalhos");
+    var caminho_atual = trabalho_caminho(atual); 
+
+    image.src = caminho_atual;      
+
+    // Atualiza caption da label
+    document.getElementById("label_Trabalhos").innerHTML = trabalho_caption(atual);
+
+    // salva nova posição atual da foto
+    save('trabalho',atual);
+}
+
+// Carrega Foto seguinte em relação a atual - REUNIÒES
+function proximo()
+{
+    // Carrega posição atual da foto
+    var atual = load('trabalho');
+
+    // Carrega quantidade máxima de fotos
+    var maximo = load('quantidade_maxima_de_fotos')
+
+    // Se a foto atual for a última (5) volta para a primeira
+    if(atual == maximo)
+    {
+        atual = 1;
+    }
+    else // Somar mais 1 para seguir para a próxima imagem
+    {
+        atual = atual + 1;
+    }
+
+    // Carrega Elemento da imagem
+    var image = document.getElementById("img_Trabalhos");  
+
+    image.classList.add('Efeito_troca_imagem_desaparece');
+
+    setTimeout(function () {
+        image.classList.remove('Efeito_troca_imagem_desaparece');
+        image.src = trabalho_caminho(atual);
+    },2000);
+
+    image.classList.add('Efeito_troca_imagem_aparece');
+
+    setTimeout(function () {
+        image.classList.remove('Efeito_troca_imagem_aparece');
+        image.src = trabalho_caminho(atual);
+    },2000);
+
+
+    // Atualiza caption da label
+    document.getElementById("label_Trabalhos").innerHTML = trabalho_caption(atual);
+
+    // salva nova posição atual da foto
+    save('trabalho',atual);
+}
+
+// Carrega caminho da imagem pertinente a imagem - REUNIÒES
+function trabalho_caminho(N_trabalho)
+{
+   var caminho = '';
+
+    switch (N_trabalho) {
+       
+        case 1:
+            caminho = "Imagens/Trabalhos_1.jpg";
+        break;
+
+        case 2:
+            caminho = "Imagens/Trabalhos_2.jpg";
+        break;
+
+        case 3:
+            caminho = "Imagens/Trabalhos_3.jpg";
+        break;
+
+        case 4:
+            caminho = "Imagens/Trabalhos_4.jpg";
+        break;
+
+        case 5:
+            caminho = "Imagens/Trabalhos_5.jpg";
+        break;           
+      }
+
+    return(caminho);
+}
+
+// Carrega caption da label referente a imagem - REUNIÒES
+function trabalho_caption(N_trabalho)
+{
+    var caption = '';
+
+    switch (N_trabalho) {
+       
+        case 1:
+            caption = "Trabalho 1";
+        break;
+
+        case 2:
+            caption = "Trabalho 2";
+        break;
+
+        case 3:
+            caption = "Trabalho 3";
+        break;
+
+        case 4:
+            caption = "Trabalho 4";
+        break;
+
+        case 5:
+            caption = "Trabalho 5";
+        break;           
+      }    
+
+    return(caption);
+}
+
+
+// Salva valor de variaval para uso posterior a executação do javascript
+function save(nome,valor)
+{
+    // Salvar informação localmente
+    localStorage.setItem(nome,valor);
+}
+
+// Carrega valor de variaval que se perdeu após a executação do javascript
+function load(nome)
+{
+    // Carregar informação local
+    var load = parseInt(localStorage.getItem(nome));
+
+    return(load);
 }
